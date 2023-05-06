1a7cfbd8a11b2a466721755c370666f98ed4c710
AshwiniKhedekar <ashwinikhedekar30@gmail.com>
2023-05-06 19:07:26 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "~> 0.0"
+    version = "3.4.0"
 
     project_id   = var.project
     network_name = "demo-vpc"
