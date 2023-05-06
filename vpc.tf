4f0607f0932ba35681660f84f1842686b309313d
AshwiniKhedekar <ashwinikhedekar30@gmail.com>
2023-05-06 20:05:56 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,14 +3,14 @@ module "Google_network_module" {
     version = "3.4.0"
 
     project_id   = var.project
-    network_name = "demo-vpc2"
+    network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
     subnets = [
         {
             subnet_name           = "gaurav-subnet"
             subnet_ip             = "10.100.10.0/24"
-            subnet_region         = var.region
+            subnet_region         =  var.region
         }
     ]
 
